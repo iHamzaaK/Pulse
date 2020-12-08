5aed6934ac972b43a19d2d5461fc3abcb262e12a
Hamza Khan <hamzakhancs15@gmail.com>
2020-12-08 11:41:52 +0100

    Base layout constraint ratio for iphone modified

--- Pulse/Source/Helper/DesignUtility.swift
+++ Pulse/Source/Helper/DesignUtility.swift
@@ -11,8 +11,8 @@ import UIKit
 
 public class DesignUtility: NSObject {
     
-    @nonobjc static let deviceRatio:CGFloat = UIScreen.main.bounds.height / 736.0;
-    @nonobjc static let deviceRatioWN:CGFloat = (UIScreen.main.bounds.height - 64.0) / (736.0 - 64.0); // Ratio with Navigation
+    @nonobjc static let deviceRatio:CGFloat = UIScreen.main.bounds.height / 812;
+    @nonobjc static let deviceRatioWN:CGFloat = (UIScreen.main.bounds.height - 64.0) / (812 - 64.0); // Ratio with Navigation
     
     /// Bool flag for device type.
     @nonobjc public static let isIPad:Bool = UIDevice.current.userInterfaceIdiom == .pad;
@@ -20,7 +20,7 @@ public class DesignUtility: NSObject {
     
     // addition by waqas to set layout on iPhone X (Dimensions: 1125px x 2436px (375pt x 812pt @3x).)
     @nonobjc static let deviceRatioIphone:CGFloat =
-        (UIScreen.main.bounds.height > 736.0 ? 667.0 : UIScreen.main.bounds.height ) / 736.0;
+        (UIScreen.main.bounds.height > 812 ? 736.0 : UIScreen.main.bounds.height ) / 812;
     
     @nonobjc static let deviceRatioIpad:CGFloat = UIScreen.main.bounds.height / 1194;
     
