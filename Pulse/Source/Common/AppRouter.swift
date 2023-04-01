5c516bd83362c85f8acb7a82079b03ace16591d5
Hamza Khan <hamzakhancs15@gmail.com>
2023-04-01 02:04:39 +0200

    lint common files

--- Pulse/Source/Common/AppRouter.swift
+++ Pulse/Source/Common/AppRouter.swift
@@ -8,102 +8,99 @@
 
 import UIKit
 import SlideMenuControllerSwift
-class AppRouter{
-    private static var window : UIWindow?
-    private static var rightMenuWidth: CGFloat = 0.8
-    private static var navController : BaseNavigationViewController = BaseNavigationViewController()
-    static var slideMenu : SlideMenuController!{
-        didSet{
-            if DesignUtility.isIPad{
-                rightMenuWidth = 0.45
-            }
-            slideMenu.changeRightViewWidth(UIScreen.main.bounds.width * rightMenuWidth)
-            SlideMenuOptions.panFromBezel = false
-            SlideMenuOptions.rightPanFromBezel = false
-            SlideMenuOptions.contentViewScale = 1.0
-        }
-    }
-    static func createRoute(window : UIWindow) {
-        SlideMenuOptions.opacityViewBackgroundColor = .white
-        self.window = window
-        self.window!.backgroundColor = UIColor.white
-        let vc : UIViewController! =  SplashBuilder.build()
-        self.navController.navigationBar.isHidden = true
-        self.navController.viewControllers = [vc]
-        let rightVC = RightMenuBuilder.build()
-        slideMenu = SlideMenuController(mainViewController: self.navController, rightMenuViewController: rightVC)
-        self.window?.rootViewController = slideMenu
-        self.window?.makeKeyAndVisible()
-        //
-        
-    }
-    static func createInitialRoute(vc: UIViewController){
-        self.navController.viewControllers = [vc]
-    }
-    
-    static func showHideRightMenu(){
-        if slideMenu.isRightOpen(){
-            slideMenu.closeRight()
-        }
-        else{
-            slideMenu.openRight()
-        }
-    }
-    static func pop(){
-        slideMenu.closeRight()
-        self.navController.popViewController(animated: true);
-        
-    }
-    static func logout(){
-        slideMenu.closeRight()
-        self.createInitialRoute(vc: LoginBuilder.build())
-        ArchiveUtil.deleteSession()
-        
-    }
-    static func goToLogin(){
-        if slideMenu.isRightOpen(){
-            slideMenu.closeRight()
-        }
-        let loginVC = LoginBuilder.build()
-        self.navController.viewControllers = [loginVC]
-    }
-    static func goToHomeFromRightMenu(){
-        slideMenu.closeRight()
-        
-        self.navController.popToRootViewController(animated: true)
-        
-    }
-    static func goToSpecificController(vc : UIViewController){
-        slideMenu.closeRight()
-        
-        let stackVCs = self.navController.viewControllers
-        
-        if self.navController.visibleViewController?.restorationIdentifier != vc.restorationIdentifier{
-            if stackVCs.contains(vc){
-                self.navController.popToViewController(vc, animated: true)
-            }
-            else{
-                self.navController.pushViewController(vc, animated: true)
-            }
-        }
-        
-    }
-    static func presentControllerForNotification(vc: UIViewController){
-        slideMenu.closeRight()
-        let navigationCont = UINavigationController.init()
-        navigationCont.navigationBar.isHidden = true
-        navigationCont.viewControllers = [vc]
-        self.window?.rootViewController?.present(navigationCont, animated: true, completion: {
-            
-        })
 
+class AppRouter{
+  private static var window : UIWindow?
+  private static var rightMenuWidth: CGFloat = 0.8
+  private static var navController : BaseNavigationViewController = BaseNavigationViewController()
+
+  static var slideMenu : SlideMenuController!{
+    didSet{
+      if DesignUtility.isIPad{
+        rightMenuWidth = 0.45
+      }
+      slideMenu.changeRightViewWidth(UIScreen.main.bounds.width * rightMenuWidth)
+      SlideMenuOptions.panFromBezel = false
+      SlideMenuOptions.rightPanFromBezel = false
+      SlideMenuOptions.contentViewScale = 1.0
     }
-    
-    
-    //    static func goToUserDashboard(dashboardData : DashboardRepo){
-    //        let dashboardVC = DashboardBuilder.build(dashboardRepo: dashboardData)
-    //        self.navController.viewControllers = [dashboardVC]
-    //    }
-   
+  }
+
+  static func createRoute(window : UIWindow) {
+    SlideMenuOptions.opacityViewBackgroundColor = .white
+    self.window = window
+    self.window!.backgroundColor = UIColor.white
+    let vc : UIViewController! =  SplashBuilder.build()
+    self.navController.navigationBar.isHidden = true
+    self.navController.viewControllers = [vc]
+    let rightVC = RightMenuBuilder.build()
+    slideMenu = SlideMenuController(mainViewController: self.navController, rightMenuViewController: rightVC)
+    self.window?.rootViewController = slideMenu
+    self.window?.makeKeyAndVisible()
+  }
+
+  static func createInitialRoute(vc: UIViewController){
+    self.navController.viewControllers = [vc]
+  }
+
+  static func showHideRightMenu(){
+    if slideMenu.isRightOpen(){
+      slideMenu.closeRight()
+    } else{
+      slideMenu.openRight()
+    }
+  }
+
+  static func pop(){
+    slideMenu.closeRight()
+    self.navController.popViewController(animated: true);
+
+  }
+
+  static func logout(){
+    slideMenu.closeRight()
+    self.createInitialRoute(vc: LoginBuilder.build())
+    ArchiveUtil.deleteSession()
+
+  }
+
+  static func goToLogin(){
+    if slideMenu.isRightOpen(){
+      slideMenu.closeRight()
+    }
+    let loginVC = LoginBuilder.build()
+    self.navController.viewControllers = [loginVC]
+  }
+
+  static func goToHomeFromRightMenu(){
+    slideMenu.closeRight()
+
+    self.navController.popToRootViewController(animated: true)
+
+  }
+
+  static func goToSpecificController(vc : UIViewController){
+    slideMenu.closeRight()
+
+    let stackVCs = self.navController.viewControllers
+
+    if self.navController.visibleViewController?.restorationIdentifier != vc.restorationIdentifier{
+      if stackVCs.contains(vc){
+        self.navController.popToViewController(vc, animated: true)
+      }
+      else{
+        self.navController.pushViewController(vc, animated: true)
+      }
+    }
+
+  }
+
+  static func presentControllerForNotification(vc: UIViewController){
+    slideMenu.closeRight()
+    let navigationCont = UINavigationController.init()
+    navigationCont.navigationBar.isHidden = true
+    navigationCont.viewControllers = [vc]
+    self.window?.rootViewController?.present(navigationCont, animated: true, completion: {})
+  }
 }
 
--- Pulse/Source/Common/Enum.swift
+++ Pulse/Source/Common/Enum.swift
@@ -9,181 +9,187 @@
 import UIKit
 
 enum Storyboards : String{
-    case auth = "Authentication"
-    case myProfile = "MyProfile"
-    case dashboard = "Dashboard"
-    case forgetPassword = "ForgetPassword"
-    case confirmPassword = "ConfirmPassword"
-    case rightMenu = "RightMenu"
-    case settings = "Settings"
-    case fullArticle = "FullArticle"
-    case splash = "Splash"
-    case aboutus = "AboutUs"
-    case comments = "Comments"
-    case videos = "Videos"
-    case quotes = "Quotes"
-    case createPost = "CreatePost"
-    case search = "Search"
-    case likes = "Likes"
-    case articleListing = "ArticleListing"
-    case notificationListing = "NotificationListing"
-    case policy = "Policy"
+  case auth = "Authentication"
+  case myProfile = "MyProfile"
+  case dashboard = "Dashboard"
+  case forgetPassword = "ForgetPassword"
+  case confirmPassword = "ConfirmPassword"
+  case rightMenu = "RightMenu"
+  case settings = "Settings"
+  case fullArticle = "FullArticle"
+  case splash = "Splash"
+  case aboutus = "AboutUs"
+  case comments = "Comments"
+  case videos = "Videos"
+  case quotes = "Quotes"
+  case createPost = "CreatePost"
+  case search = "Search"
+  case likes = "Likes"
+  case articleListing = "ArticleListing"
+  case notificationListing = "NotificationListing"
+  case policy = "Policy"
   case filter = "Filter"
-
 }
 
 enum ViewControllersIdentifier: String{
   case filter = "FilterViewController"
+  case policy = "PolicyViewController"
+  case notificationListing = "NotificationListingViewController"
+  case articleListing = "ArticleListingViewController"
+  case likes = "LikesViewController"
+  case otp = "OTPViewController"
+  case search = "SearchViewController"
+  case createPost = "CreatePostViewController"
+  case videos = "VideosViewController"
+  case quotes = "QuotesViewController"
+  case comments = "CommentsViewController"
+  case interest = "InterestViewController"
+  case bookmark = "BookmarksViewController"
+  case categories = "CategoriesViewController"
+  case fullArticle = "FullArticleViewController"
+  case aboutus = "AboutUsViewController"
+  case splash = "SplashViewController"
+  case login = "LoginViewController"
+  case dashboard = "DashboardViewController"
+  case forgetPassword = "ForgetPasswordViewController"
+  case confirmPassword = "ConfirmPasswordViewController"
+  case rightMenu = "RightMenuViewController"
+  case settings = "SettingsViewController"
+  case myProfile = "MyProfileViewController"
+  case topStories = "TopStoriesViewController"
+  case myNews = "MyNewsViewController"
+}
 
-    case policy = "PolicyViewController"
-    case notificationListing = "NotificationListingViewController"
-    case articleListing = "ArticleListingViewController"
-    case likes = "LikesViewController"
-    case otp = "OTPViewController"
-    case search = "SearchViewController"
-    case createPost = "CreatePostViewController"
-    case videos = "VideosViewController"
-    case quotes = "QuotesViewController"
-    case comments = "CommentsViewController"
-    case interest = "InterestViewController"
-    case bookmark = "BookmarksViewController"
-    case categories = "CategoriesViewController"
-    case fullArticle = "FullArticleViewController"
-    case aboutus = "AboutUsViewController"
-    case splash = "SplashViewController"
-    case login = "LoginViewController"
-    case dashboard = "DashboardViewController"
-    case forgetPassword = "ForgetPasswordViewController"
-    case confirmPassword = "ConfirmPasswordViewController"
-    case rightMenu = "RightMenuViewController"
-    case settings = "SettingsViewController"
-    case myProfile = "MyProfileViewController"
-    case topStories = "TopStoriesViewController"
-    case myNews = "MyNewsViewController"
-    
-}
 enum LocalizeFiles : String{
-    case button = "Button"
-    case label = "Label"
-    case txtFields = "TextFields"
-    case text = "Text"
-    case navigationTitle = "NavigationTitle"
+  case button = "Button"
+  case label = "Label"
+  case txtFields = "TextFields"
+  case text = "Text"
+  case navigationTitle = "NavigationTitle"
 }
+
 enum Gender : Int{
-    case female = 0
-    case male
+  case female = 0
+  case male
 }
 
 enum articleListingType : Int{
-    case topStories = 1
-    case myNews
-    case categories
-    case bookmarks
-    case interest
-    case videos
+  case topStories = 1
+  case myNews
+  case categories
+  case bookmarks
+  case interest
+  case videos
 }
+
 enum CellTypes{
-    case dropDown
-    case textField
-    case button
-    case facebook
-    case label
-    case register
-    case timeSlots
-    case dashboard
-    case datePicker
-    case categories
-    case location
-    case restaurantFinder
-
-    func signupTextField()-> String { return signupCellIdentifier}
-    func signupFacebookField()-> String { return facebookCellIdentifier}
-    func signupLabel()-> String { return labelCellIdentifier }
-    func signupRegisteration()-> String{ return signupRegisterationCellIdentifier }
-    func signupTimeSlots()-> String{ return signupTimeSlotCellIdentifier }
-    func dashboardCell()-> String{ return dashboardCellIdentifier }
-    func restaurantFinder()-> String{ return dashboardCellIdentifier }
-
-    
+  case dropDown
+  case textField
+  case button
+  case facebook
+  case label
+  case register
+  case timeSlots
+  case dashboard
+  case datePicker
+  case categories
+  case location
+  case restaurantFinder
+  
+  func signupTextField()-> String { return signupCellIdentifier}
+  func signupFacebookField()-> String { return facebookCellIdentifier}
+  func signupLabel()-> String { return labelCellIdentifier }
+  func signupRegisteration()-> String{ return signupRegisterationCellIdentifier }
+  func signupTimeSlots()-> String{ return signupTimeSlotCellIdentifier }
+  func dashboardCell()-> String{ return dashboardCellIdentifier }
+  func restaurantFinder()-> String{ return dashboardCellIdentifier }
 }
+
 enum KeyboardTypes{
-    case email
-    case normal
-    case phonePad
+  case email
+  case normal
+  case phonePad
 }
+
 enum StatusCode : Int{
-    case success = 200
-    case authExpired = 401
-    case error = 1002
+  case success = 200
+  case authExpired = 401
+  case error = 1002
 }
+
 enum ErrorDescription : String {
-    case errorTitle = "Error"
-    case invalidEmail = "Entered wrong Email ID"
-    case invalidPasswordd = "Entered a wrong password"
-    case passwordMismatch = "Password and confirm password doesn't match."
-    case invalidData = "Input not correct"
-    case invalidDate = "Time To should be greater then time from"
-    case invalidCredentials = "Invalid Credentials"
-    case emptyData = "Field cannot be empty"
-    case unknown = "Something went wrong. Please try again. Thank you!"
-    
+  case errorTitle = "Error"
+  case invalidEmail = "Entered wrong Email ID"
+  case invalidPasswordd = "Entered a wrong password"
+  case passwordMismatch = "Password and confirm password doesn't match."
+  case invalidData = "Input not correct"
+  case invalidDate = "Time To should be greater then time from"
+  case invalidCredentials = "Invalid Credentials"
+  case emptyData = "Field cannot be empty"
+  case unknown = "Something went wrong. Please try again. Thank you!"
+  
 }
 
 enum userTypes : String{
-    case author = "Author"
-    case viewer = "Viewer"
+  case author = "Author"
+  case viewer = "Viewer"
 }
+
 enum userStatus : String{
-    case active = "Active"
-    case notVerify = "Not_Verify"
-    case suspend = "Suspended"
+  case active = "Active"
+  case notVerify = "Not_Verify"
+  case suspend = "Suspended"
 }
 
 enum EmailErrors: Error {
-    case invalidEmail
+  case invalidEmail
 }
+
 enum PasswordErrors: Error {
-    case invalidPassword
-    case passwordMismatch
+  case invalidPassword
+  case passwordMismatch
 }
+
 enum textFieldValidationErrors : Error {
-    case emptyValue
-    case invalidValue
-    case timeInvalid
+  case emptyValue
+  case invalidValue
+  case timeInvalid
 }
 
 enum webserviceError : Error{
-    case dataNotSerialized
+  case dataNotSerialized
 }
-enum appColors {
-    case green
-    case adBlue
-    case headerBlue
-    case grayBorderColor
-    
-    var colorStatus : UIColor{
-        get{
-            switch self {
-            case .green:
-                return Utilities.hexStringToUIColor(hex: "44ad47")
-            case .adBlue:
-                return Utilities.hexStringToUIColor(hex: "005b72")
-            case .headerBlue:
-                return Utilities.hexStringToUIColor(hex: "5b92fa")
-            case .grayBorderColor:
-                return Utilities.hexStringToUIColor(hex: "e3e3e3")
 
-            }
-        }
+enum appColors {
+  case green
+  case adBlue
+  case headerBlue
+  case grayBorderColor
+  
+  var colorStatus : UIColor{
+    get{
+      switch self {
+      case .green:
+        return Utilities.hexStringToUIColor(hex: "44ad47")
+      case .adBlue:
+        return Utilities.hexStringToUIColor(hex: "005b72")
+      case .headerBlue:
+        return Utilities.hexStringToUIColor(hex: "5b92fa")
+      case .grayBorderColor:
+        return Utilities.hexStringToUIColor(hex: "e3e3e3")
+        
+      }
     }
+  }
 }
+
 enum ImagesTextField : String{
-    case email = "email"
-    case password = "password"
-    case name = "name"
-    case website = "worldwide"
-    case phone = "phone"
-    case firmName = "home"
+  case email = "email"
+  case password = "password"
+  case name = "name"
+  case website = "worldwide"
+  case phone = "phone"
+  case firmName = "home"
 }
 
 enum DateTimeFilter : Int {
@@ -192,7 +198,7 @@ enum DateTimeFilter : Int {
   case week
   case month
   case year
-
+  
   static func allFilters() -> [String]{
     return ["Any","Last 24H","Last Week","Last Month","Last Year"]
   }
