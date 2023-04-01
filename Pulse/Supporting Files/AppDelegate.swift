23b4615ceece084bbd16e718eb406fe16bd2cd76
Hamza Khan <hamzakhancs15@gmail.com>
2023-04-01 02:08:16 +0200

    lint appdelegate and scenedelegate

--- Pulse/Supporting Files/AppDelegate.swift
+++ Pulse/Supporting Files/AppDelegate.swift
@@ -19,11 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
   let notificationCenter = UNUserNotificationCenter.current()
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
     IQKeyboardManager.shared.enable = true
     registerForPushNotifications()
     notificationCenter.delegate = self
-
 
     let appearance = UITabBarItem.appearance()
     let attributes = [NSAttributedString.Key.font:UIFont(name: "Montserrat-Medium", size: DesignUtility.convertToRatio(10, sizedForIPad:  DesignUtility.isIPad, sizedForNavi: false))]
@@ -40,16 +38,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
   // MARK: UISceneSession Lifecycle
 
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
 
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-  }
   func getNotificationSettings() {
     UNUserNotificationCenter.current().getNotificationSettings { settings in
       print("Notification settings: \(settings)")
@@ -66,42 +57,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
     completionHandler([.banner, .sound])
 
   }
+
   func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
     let userInfo = response.notification.request.content.userInfo
-    //        let userInfo = ["identifier":response.notification.request.identifier]
     guard let aps = userInfo["aps"] as? [String: AnyObject] else {
       return
     }
+
     guard let alert = aps["alert"] as? [String: AnyObject] else {
       return
     }
-    print(alert["title"])
+
     let articleID = alert["body"] as! String
     let application = UIApplication.shared
 
-    if(application.applicationState == .active){
+    if(application.applicationState == .active) {
       print("user tapped the notification bar when the app is in foreground")
       AppRouter.presentControllerForNotification(vc: FullArticleBuilder.build(articleID: articleID, headerType: .clearNavBar))
     }
 
-    else if(application.applicationState == .inactive)
-    {
+    else if(application.applicationState == .inactive) {
       AppRouter.presentControllerForNotification(vc: FullArticleBuilder.build(articleID: articleID, headerType: .clearNavBar))
-
       print("user tapped the notification bar when the app is in background")
     }
     else {
       if ArchiveUtil.getUserToken() != ""{
-        //                Timer.scheduledTimer(withTimeInterval: 10.0, repeats: false) { (_) in
         AppRouter.presentControllerForNotification(vc: FullArticleBuilder.build(articleID: articleID, headerType: .clearNavBar))
-        //                }
       }
     }
     completionHandler()
 
   }
   func registerForPushNotifications() {
-
     UNUserNotificationCenter.current()
       .requestAuthorization(
         options: [.alert, .sound, .badge]) { [weak self] granted, _ in
@@ -126,44 +113,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
     print("Failed to register: \(error)")
   }
 
-  //    func application( _ application : UIApplication, continue userActivity: NSUserActivity , restorationHandler: @escaping( [UIUserActivityRestoring]?)->Void)->Bool{
-  //
-  //        guard let  url = userActivity.webpageURL else { return false }
-  //
-  //
-  //        return false
-  //    }
-  //pulseapp:article?id=1"
-  //    func application(_ application: UIApplication,
-  //                     open url: URL,
-  //                     options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
-  //
-  //        // Determine who sent the URL.
-  //        let sendingAppID = options[.sourceApplication]
-  //        print("source application = \(sendingAppID ?? "Unknown")")
-  //
-  //        // Process the URL.
-  //        guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
-  //              let article = components.path,
-  //              let params = components.queryItems else {
-  //                  print("Invalid URL or article path missing")
-  //                  return false
-  //              }
-  //
-  //        if let articleId = params.first(where: { $0.name == "id" })?.value {
-  //            print("article path = \(article)")
-  //            print("article Id = \(articleId)")
-  //            ArchiveUtil.saveArticleIDFromAppUrlLink(articleId: articleId)
-  //
-  //            return true
-  //        } else {
-  //            print("article Id missing")
-  //            return false
-  //        }
-  //        // login page check session
-  //        // check if archive util  have this
-  //        // go to article detail with article id
-  //    }
   func application(_ app: UIApplication, open url: URL,
                    options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     if let scheme = url.scheme,
@@ -174,8 +123,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
       URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.forEach {
         parameters[$0.name] = $0.value
       }
-
-      //            redirect(to: view, with: parameters)
     }
     return true
   }
--- Pulse/Supporting Files/SceneDelegate.swift
+++ Pulse/Supporting Files/SceneDelegate.swift
@@ -8,115 +8,54 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
 
-    var window: UIWindow?
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    self.window = UIWindow(windowScene: windowScene)
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        self.window = UIWindow(windowScene: windowScene)
-        
-        AppRouter.createRoute(window: self.window!)
-//        self.scene(scene, openURLContexts: connectionOptions.urlContexts)
-        if let userActivity = connectionOptions.userActivities.first {
-            if let incomingURL = userActivity.webpageURL {
-                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+2) {
-
-                                handleURL(url: incomingURL)
-                        }
-                
-            }
+    AppRouter.createRoute(window: self.window!)
+    if let userActivity = connectionOptions.userActivities.first {
+      if let incomingURL = userActivity.webpageURL {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+2) {
+          handleURL(url: incomingURL)
         }
-//        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+2) {
-//            if let url = connectionOptions.urlContexts.first?.url {
-//
-//                handleURL(url: url)
-//            }
-//        }
-        
+      }
     }
-    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
-        if let url = userActivity.webpageURL {
-            // ...
-            handleURL(url: url)
-        }
-    }
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
+  }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+  func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+    if let url = userActivity.webpageURL {
+      handleURL(url: url)
     }
+  }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    guard let url = URLContexts.first?.url else {
+      return
     }
+    handleURL(url: url)
+  }
+}
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
+private func handleURL(url : URL){
+  guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
+        let article = components.path,
+        let params = components.queryItems else {
+    print("Invalid URL or article path missing")
+    return
+  }
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+  if let articleId = params.first(where: { $0.name == "id" })?.value {
+    print("article path = \(article)")
+    print("article Id = \(articleId)")
+    ArchiveUtil.saveArticleIDFromAppUrlLink(articleId: articleId)
+    if articleId != ""{
+      let vc = FullArticleBuilder.build(articleID: articleId, headerType: .backButtonWithTitle)
+      AppRouter.goToSpecificController(vc: vc)
     }
-
-    
-    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        guard let url = URLContexts.first?.url else {
-            return
-        }
-//        if let scheme = url.scheme,
-//           scheme.localizedCaseInsensitiveCompare("pulseapp") == .orderedSame,
-//           let view = url.host {
-//
-//            var parameters: [String: String] = [:]
-//            URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.forEach {
-//                parameters[$0.name] = $0.value
-//            }
-//            print(parameters)
-            handleURL(url: url)
-//            redirect(to: view, with: parameters)
-        }
-        
-    }
-    private func handleURL(url : URL){
-        guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
-              let article = components.path,
-              let params = components.queryItems else {
-                  print("Invalid URL or article path missing")
-                  return
-              }
-        
-        if let articleId = params.first(where: { $0.name == "id" })?.value {
-            print("article path = \(article)")
-            print("article Id = \(articleId)")
-            ArchiveUtil.saveArticleIDFromAppUrlLink(articleId: articleId)
-            
-            //                            let articleID = ArchiveUtil.getArticleIDFromAppUrlLink()
-            if articleId != ""{
-                let vc = FullArticleBuilder.build(articleID: articleId, headerType: .backButtonWithTitle)
-                AppRouter.goToSpecificController(vc: vc)
-                
-                
-            }
-            return
-        } else {
-            print("article Id missing")
-            return
-        }
-    
+    return
+  }
+  return
 }
 
