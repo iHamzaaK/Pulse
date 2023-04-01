d012d06e80512f4e380372b6fe44ed001c6dde4b
Hamza Khan <hamzakhancs15@gmail.com>
2023-04-01 02:10:33 +0200

    lint general repo

--- Pulse/Source/Model/General.swift
+++ Pulse/Source/Model/General.swift
@@ -9,22 +9,20 @@ import Foundation
 import Foundation
 
 class GeneralRepoModel : Codable {
-    let success : Bool?
-    let statusCode : Int?
-    let message : String?
-
-
-    enum CodingKeys: String, CodingKey {
-        case statusCode = "statusCode"
-        case message = "message"
-        case success = "success"
-    }
-    required init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        statusCode = try values.decodeIfPresent(Int.self, forKey: .statusCode) ?? Int()
-        message = try values.decodeIfPresent(String.self, forKey: .message) ?? String()
-        success = try values.decodeIfPresent(Bool.self, forKey: .success) ?? Bool()
-    }
-
-
+  let success : Bool?
+  let statusCode : Int?
+  let message : String?
+  
+  enum CodingKeys: String, CodingKey {
+    case statusCode = "statusCode"
+    case message = "message"
+    case success = "success"
+  }
+  
+  required init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    statusCode = try values.decodeIfPresent(Int.self, forKey: .statusCode) ?? Int()
+    message = try values.decodeIfPresent(String.self, forKey: .message) ?? String()
+    success = try values.decodeIfPresent(Bool.self, forKey: .success) ?? Bool()
+  }
 }
