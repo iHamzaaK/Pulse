b78371ce12618998f97e92d3e938e4619cf222c6
Hamza Khan <hamzakhancs15@gmail.com>
2023-04-01 02:14:10 +0200

    lint fav repo

--- Pulse/Source/Repository/Favourites/FavouritesRepository.swift
+++ Pulse/Source/Repository/Favourites/FavouritesRepository.swift
@@ -5,7 +5,6 @@
 //  Created by Hamza Khan on 02/12/2020.
 //
 
-import Foundation
 protocol FavouritesRepository{
-    func changeOldPassword(resetCode : String,email : String,password : String, completionHandler: @escaping (Bool, String) -> Void)
+  func changeOldPassword(resetCode : String,email : String,password : String, completionHandler: @escaping (Bool, String) -> Void)
 }
